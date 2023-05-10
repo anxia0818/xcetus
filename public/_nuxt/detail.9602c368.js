9fed8c2e38a98873105904f742da3f7cc2b5c1f3
yujianming <anxia0818@163.com>
2023-05-10 15:07:20 +0800

    update

--- public/_nuxt/detail.9602c368.js
+++
@@ -1,1 +0,0 @@
-import{eZ as e}from"./entry.9449c7e1.js";const l=e((t,d)=>{const o=document.querySelector(".body-container-bg");console.log(o,"body##"),o&&o.scrollTo({top:0})});export{l as default};
---
+++ public/_nuxt/icon-Rewards.b08790bb.js
@@ -0,0 +1,1 @@
+import"./entry.744c6c12.js";const e=""+new URL("icon-Rewards.add64e7c.png",import.meta.url).href;export{e as _};
