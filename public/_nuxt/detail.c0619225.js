0a76b324782c57733dada65d889cad23e4ef71cc
yujianming <anxia0818@163.com>
2023-05-10 15:57:07 +0800

    update

--- public/_nuxt/detail.c0619225.js
+++
@@ -1,1 +0,0 @@
-import{eZ as e}from"./entry.744c6c12.js";const l=e((t,d)=>{const o=document.querySelector(".body-container-bg");console.log(o,"body##"),o&&o.scrollTo({top:0})});export{l as default};
---
+++ public/_nuxt/detail.c0def03e.js
@@ -0,0 +1,1 @@
+import{eZ as e}from"./entry.517e5b8b.js";const l=e((t,d)=>{const o=document.querySelector(".body-container-bg");console.log(o,"body##"),o&&o.scrollTo({top:0})});export{l as default};
