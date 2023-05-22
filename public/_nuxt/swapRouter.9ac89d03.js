bd7cfe75c252668d12060e29e3aaf6344d4ce1bb
yujianming <anxia0818@163.com>
2023-05-22 20:59:32 +0800

    update

---
+++ public/_nuxt/swapRouter.9ac89d03.js
@@ -0,0 +1,1 @@
+import{a0 as a}from"./entry.68442341.js";import{b as e}from"./pool.25e4b096.js";const u=a("swapRouter",{state:()=>({swapRouterContract:{}}),actions:{initSwapRouter(t,o,r){this.swapRouterContract=e(t),this.swapRouterContract.initSwapRouter(o,r),console.log(this.swapRouterContract,"===>this.swapRouterContract")}}});export{u};
