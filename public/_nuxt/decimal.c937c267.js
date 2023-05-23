7c417e0554045e5cfcaa736d63e02c493efe5529
yujianming <anxia0818@163.com>
2023-05-23 14:58:00 +0800

    update

--- public/_nuxt/decimal.c937c267.js
+++
@@ -1,1 +0,0 @@
-import{D as o}from"./pool.5472dae1.js";o.config({precision:64,rounding:o.ROUND_DOWN,toExpNeg:-64,toExpPos:64});
---
+++ public/_nuxt/detail.f7ae4a1f.js
@@ -0,0 +1,1 @@
+import{e_ as e}from"./entry.75ea8930.js";const l=e((t,d)=>{const o=document.querySelector(".body-container-bg");console.log(o,"body##"),o&&o.scrollTo({top:0})});export{l as default};
