ce705f51dc03d003e7fa941f2ea829d502b6a218
yujianming <anxia0818@163.com>
2023-05-24 20:16:58 +0800

    update

---
+++ public/_nuxt/detail.e675789b.js
@@ -0,0 +1,1 @@
+import{e_ as e}from"./entry.9295917f.js";const l=e((t,d)=>{const o=document.querySelector(".body-container-bg");console.log(o,"body##"),o&&o.scrollTo({top:0})});export{l as default};
