46813f999d03adb2678949eedc868028b3f24fb0
anxia0818 <1298003858@qq.com>
2023-08-08 16:02:21 +0800

    update

---
+++ public/_nuxt/detail.9fa75f3f.js
@@ -0,0 +1,1 @@
+import{fa as e}from"./entry.06373f20.js";const l=e((t,d)=>{const o=document.querySelector(".body-container-bg");console.log(o,"body##"),o&&o.scrollTo({top:0})});export{l as default};
---
+++ public/_nuxt/icon-Rewards.eaf1d67e.js
@@ -0,0 +1,1 @@
+import"./entry.06373f20.js";const e=""+new URL("icon-Rewards.add64e7c.png",import.meta.url).href;export{e as _};
