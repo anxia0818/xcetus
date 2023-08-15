0d62c5c68d29f2ce6410c3d93a8233908809318a
anxia0818 <1298003858@qq.com>
2023-08-15 17:14:38 +0800

    update

--- public/_nuxt/detail.8e25796e.js
+++
@@ -1,1 +0,0 @@
-import{af as e}from"./entry.95a6c2a7.js";const l=e((t,d)=>{const o=document.querySelector(".body-container-bg");console.log(o,"body##"),o&&o.scrollTo({top:0})});export{l as default};
