bb2defcb3256f71471250024131694c1fd132787
anxia0818 <1298003858@qq.com>
2023-08-22 14:56:05 +0800

    update

--- public/_nuxt/detail.34bf9599.js
+++
@@ -1,1 +0,0 @@
-import{fa as e}from"./entry.a5afa453.js";const l=e((t,d)=>{const o=document.querySelector(".body-container-bg");console.log(o,"body##"),o&&o.scrollTo({top:0})});export{l as default};
---
+++ public/_nuxt/lock.d1558459.js
@@ -0,0 +1,1 @@
+import"./entry.ada93672.js";const r=""+new URL("lock.aebbc2c5.png",import.meta.url).href;export{r as _};
