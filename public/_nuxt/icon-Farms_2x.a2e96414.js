bdcf9b92b8b833c5b19bb3cc32ddab73d8d326a4
anxia0818 <1298003858@qq.com>
2023-09-12 21:01:34 +0800

    update

--- public/_nuxt/icon-Farms_2x.a2e96414.js
+++
@@ -1,1 +0,0 @@
-import"./entry.4958ad73.js";const o=""+new URL("icon-Farms_2x.a9e99fb2.png",import.meta.url).href;export{o as _};
---
+++ public/_nuxt/img-no-Positions_2x.0ed8fc9b.js
@@ -0,0 +1,1 @@
+import"./entry.0575cc71.js";const i=""+globalThis.__publicAssetsURL("image/img-no-Positions@2x.png"),o=""+globalThis.__publicAssetsURL("sui-image/img-no-Positions@2x.png");export{i as _,o as a};
