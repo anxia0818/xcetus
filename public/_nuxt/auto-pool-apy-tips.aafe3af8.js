f6965e8092446f54feabf20c5befccc916f8a7f0
anxia0818 <1298003858@qq.com>
2023-09-21 17:39:44 +0800

    update

--- public/_nuxt/auto-pool-apy-tips.aafe3af8.js
+++
@@ -1,1 +0,0 @@
-import{e,o,f as t}from"./entry.fb7fc82e.js";const _={},s={class:"apy-hover"};function n(a,c){return o(),t("div",s," APY is estimated according to the trading fees and rewards earned over the past 24 hours with daily compounding to be considered. ")}const r=e(_,[["render",n],["__scopeId","data-v-a1bebdee"]]);export{r as _};
