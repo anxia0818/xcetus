771f31fdf312b6405d929c1868c2d575a8919a06
anxia0818 <1298003858@qq.com>
2023-09-22 19:29:33 +0800

    update

--- public/_nuxt/icon-Farms_2x.ac9fa01b.js
+++
@@ -1,1 +0,0 @@
-import"./entry.f9dc8c81.js";const o=""+new URL("icon-Farms_2x.a9e99fb2.png",import.meta.url).href;export{o as _};
--- public/_nuxt/icon_autopools_2x.ebe6738b.js
+++
@@ -1,1 +0,0 @@
-import"./entry.f9dc8c81.js";const s=""+globalThis.__publicAssetsURL("sui-image/icon_autopools@2x.png");export{s as _};
