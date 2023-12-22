87625e731d40a4c3c892f26b84a6bfa283767e9a
anxia0818 <1298003858@qq.com>
2023-12-22 15:50:31 +0800

    update

---
+++ dist/assets/useGetUserBalance.query-fc403b4a.js
@@ -0,0 +1,1 @@
+import{Q as t}from"./index-46a8468e.js";import{u as c}from"./graphql-hooks-b49f6120.js";function l(o){const{loading:a,error:r,data:n}=c({variables:{owner:o},skip:!o,onError:e=>{t.error((e==null?void 0:e.message)??`Failed to fetch balance from address: ${o}`)},fetchPolicy:"no-cache"});return{loading:a,error:r,data:n}}export{l as u};
