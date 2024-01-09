b3381e193c5ca5f77f942311b0dd2e9541744d51
anxia0818 <1298003858@qq.com>
2024-01-09 20:17:40 +0800

    update

---
+++ public/_nuxt/farms.8d5a5397.js
@@ -0,0 +1,1 @@
+import"./entry.4f0b5ffa.js";const i=""+globalThis.__publicAssetsURL("sui-image/icon_farms@2x.png"),_=""+globalThis.__publicAssetsURL("sui-image/farms.gif");export{i as _,_ as a};
--- public/_nuxt/lock.ec3298dc.js
+++
@@ -1,1 +0,0 @@
-import"./entry.818b5f76.js";const r=""+new URL("lock.aebbc2c5.png",import.meta.url).href;export{r as _};
