448deb016337f9130279ed1527ad4542139356d8
yujianming <1298003858@qq.com>
2024-03-11 17:51:49 +0800

    update

--- public/_nuxt/img-connect-wallet_2x.88c4ce51.js
+++
@@ -1,1 +0,0 @@
-import"./entry.10c52014.js";const e=""+new URL("img-connect-wallet_2x.ff62feba.png",import.meta.url).href;export{e as _};
---
+++ public/_nuxt/img-no-Positions_2x.c4167ab5.js
@@ -0,0 +1,1 @@
+import"./entry.9bdd79a8.js";const i=""+globalThis.__publicAssetsURL("sui-image/img-no-Positions@2x.png");export{i as _};
