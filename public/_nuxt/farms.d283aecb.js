000a7a3dc3727cc9b91730eb134864558aba4a29
yujianming <1298003858@qq.com>
2024-03-14 19:45:22 +0800

    update

---
+++ public/_nuxt/farms.d283aecb.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./nuxt-link.a907485d.js";import{e as o,o as n,f as a,i as c,w as r,j as s}from"./entry.407a4bfc.js";const _={};function f(m,i){const e=t;return n(),a("div",null,[c(e,{to:"/farms"},{default:r(()=>[s("swap")]),_:1})])}const d=o(_,[["render",f]]);export{d as default};
---
+++ public/_nuxt/icon_rewards_2x.cc559504.js
@@ -0,0 +1,1 @@
+import"./entry.407a4bfc.js";const i=""+globalThis.__publicAssetsURL("sui-image/icon_rewards@2x.png");export{i as _};
