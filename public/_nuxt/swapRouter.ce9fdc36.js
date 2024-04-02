cd6ec9db3592e6e74d2b040411d25f94bc016811
yujianming <1298003858@qq.com>
2024-04-02 19:29:26 +0800

    update

---
+++ public/_nuxt/swapRouter.ce9fdc36.js
@@ -0,0 +1,1 @@
+import{a6 as e}from"./entry.c70dafaa.js";import{e as a}from"./pool.bc5e0169.js";const u=e("swapRouter",{state:()=>({swapRouterContract:{},isInitRouter:!0}),actions:{initSwapRouter(t,r,o){this.swapRouterContract=a(t),this.swapRouterContract.initSwapRouter(r,o),this.isInitRouter=!1}}});export{u};
