94d0f688f6c34576e927990e397f60bb71cc17cb
anxia0818 <1298003858@qq.com>
2024-04-15 14:51:37 +0800

    update

---
+++ public/_nuxt/icon_rewards_2x.8fb6fd89.js
@@ -0,0 +1,1 @@
+import"./entry.0d833121.js";const i=""+globalThis.__publicAssetsURL("sui-image/icon_rewards@2x.png");export{i as _};
---
+++ public/_nuxt/route-view.60775bdc.js
@@ -0,0 +1,1 @@
+import"./entry.0d833121.js";const i=""+globalThis.__publicAssetsURL("sui-image/route-view.png");export{i as _};
