c74af8736e9b61d6dbc273ef176976359bb5a697
anxia0818 <1298003858@qq.com>
2024-04-19 18:52:05 +0800

    update

---
+++ public/_nuxt/farms.b4c7573d.js
@@ -0,0 +1,1 @@
+import"./entry.e1fb0723.js";const i=""+globalThis.__publicAssetsURL("sui-image/farms.gif");export{i as _};
---
+++ public/_nuxt/icon_farms_2x.c71d5ccf.js
@@ -0,0 +1,1 @@
+import"./entry.e1fb0723.js";const i=""+globalThis.__publicAssetsURL("sui-image/icon_farms@2x.png");export{i as _};
