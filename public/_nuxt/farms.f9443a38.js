cf40c60d86f44ce5f8d7a1eb0cd514bf87c317b7
anxia0818 <1298003858@qq.com>
2024-04-25 18:38:02 +0800

    update

---
+++ public/_nuxt/farms.f9443a38.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./nuxt-link.4f97b488.js";import{e as o,o as n,f as a,i as c,w as r,j as s}from"./entry.3c819c28.js";const _={};function f(m,i){const e=t;return n(),a("div",null,[c(e,{to:"/farms"},{default:r(()=>[s("swap")]),_:1})])}const d=o(_,[["render",f]]);export{d as default};
