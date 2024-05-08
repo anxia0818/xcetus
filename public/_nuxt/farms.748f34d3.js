65565b24aeab1a1e9511c24240db0045c0e9c9b1
anxia0818 <1298003858@qq.com>
2024-05-08 10:59:31 +0800

    update

---
+++ public/_nuxt/farms.748f34d3.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./nuxt-link.3b58d99a.js";import{e as o,o as n,f as a,i as c,w as r,j as s}from"./entry.781e84ac.js";const _={};function f(m,i){const e=t;return n(),a("div",null,[c(e,{to:"/farms"},{default:r(()=>[s("swap")]),_:1})])}const d=o(_,[["render",f]]);export{d as default};
---
+++ public/_nuxt/route-view.142a8be9.js
@@ -0,0 +1,1 @@
+import"./entry.781e84ac.js";const i=""+globalThis.__publicAssetsURL("sui-image/route-view.png");export{i as _};
