760b757c27f311024dfd89641619a6f38fca4f80
anxia0818 <1298003858@qq.com>
2024-05-22 14:59:17 +0800

    update

---
+++ public/_nuxt/farms.2a357937.js
@@ -0,0 +1,1 @@
+import"./entry.092fa2ac.js";const i=""+globalThis.__publicAssetsURL("sui-image/farms.gif");export{i as _};
---
+++ public/_nuxt/icon_farms_2x.fbaa234d.js
@@ -0,0 +1,1 @@
+import"./entry.092fa2ac.js";const i=""+globalThis.__publicAssetsURL("sui-image/icon_farms@2x.png");export{i as _};
