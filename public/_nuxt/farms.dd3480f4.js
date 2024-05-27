c1c47527147db729b92131d00d1d2374314f2c55
anxia0818 <1298003858@qq.com>
2024-05-27 15:45:46 +0800

    update

---
+++ public/_nuxt/farms.dd3480f4.js
@@ -0,0 +1,1 @@
+import"./entry.cfe8548a.js";const i=""+globalThis.__publicAssetsURL("sui-image/farms.gif");export{i as _};
---
+++ public/_nuxt/route-view.f72f1aec.js
@@ -0,0 +1,1 @@
+import"./entry.cfe8548a.js";const i=""+globalThis.__publicAssetsURL("sui-image/route-view.png");export{i as _};
