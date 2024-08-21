0eec1121e59d051fa1ca8fa0db04005c309d7e5d
sdasda1z <anxia0818@proton.me>
2024-08-21 14:49:43 +0800

    update

--- public/_nuxt/farms.83ed36a3.js
+++
@@ -1,1 +0,0 @@
-import"./entry.d3d20915.js";const i=""+globalThis.__publicAssetsURL("sui-image/farms.gif");export{i as _};
---
+++ public/_nuxt/token-warning_2x.e5e085e9.js
@@ -0,0 +1,1 @@
+import"./entry.a748752a.js";const i=""+globalThis.__publicAssetsURL("sui-image/token-warning@2x.png");export{i as _};
