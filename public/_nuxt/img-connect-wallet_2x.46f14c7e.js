1e36decaf40048b02b5daf5005107d1107dbfaae
sdasda1z <anxia0818@proton.me>
2024-09-06 19:15:57 +0800

    update

--- public/_nuxt/img-connect-wallet_2x.46f14c7e.js
+++
@@ -1,1 +0,0 @@
-import"./entry.35d95077.js";const e=""+new URL("img-connect-wallet_2x.ff62feba.png",import.meta.url).href;export{e as _};
--- public/_nuxt/limit-beta.31f74013.js
+++
@@ -1,1 +0,0 @@
-import"./entry.35d95077.js";const s=""+globalThis.__publicAssetsURL("sui-image/limit-beta.png");export{s as _};
