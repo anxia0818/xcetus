ff279be6c8eb31356f54c60533b3a167a8f1da3a
sdasda1z <anxia0818@proton.me>
2024-09-26 17:48:10 +0800

    update

---
+++ public/_nuxt/farms.b9339d06.js
@@ -0,0 +1,1 @@
+import"./entry.3b512f86.js";const i=""+globalThis.__publicAssetsURL("sui-image/farms.gif");export{i as _};
--- public/_nuxt/farms.c942e87b.js
+++
@@ -1,1 +0,0 @@
-import"./entry.7d851ce2.js";const i=""+globalThis.__publicAssetsURL("sui-image/farms.gif");export{i as _};
